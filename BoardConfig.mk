b57e07788bb9f927d5419a84289e8b40c8cff051
Redick Lin <redicklin@google.com>
2020-04-20 18:28:53 +0800

    a6010: BOARD_CHARGER_ENABLE_SUSPEND becomes  ro.charger.enable_suspend
    Define ro.charger.enable_suspend to true as charger no
    longer uses BOARD_CHARGER_ENABLE_SUSPEND.
    
    Bug: 153937817
    Signed-off-by: Redick Lin <redicklin@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,6 @@ QCOM_BT_READ_ADDR_FROM_PROP := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
