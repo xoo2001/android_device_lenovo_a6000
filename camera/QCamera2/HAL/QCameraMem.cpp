LineageOS/android_device_lenovo_YTX703-common

QCamera2: HAL1: Call set_buffers_geometry() with actual preview dimensions
Issue:
At present, Camera HAL calling set_buffers_geometry() with stride &
scanline(padded dimensions as per GPU allignment) and then calling
set_crop() with actual dimensions to CROP the actual data from
complete buffer.
But recently EGL CROP extension has been disabled and So, Some
3rd party applications showing the entire frame which results in
green band at the top of the image.

Fix:
1) Send the actual dimensions of preview through set_buffers_geometry()
instead of stride & scanline.
2) Remove the set_crop()call from camera HAL as it's not supported by
GPU.

Change-Id: I7a5440c3ced8c6f5d107d4e00e79c7916df2fb6c
CRs-Fixed: 1051456

hardware/camera/QCamera2/HAL/QCameraMem.cpp
@@ -2147,17 +2147,9 @@ int QCameraGrallocMemory::allocate(uint8_t count, size_t /*size*/,
          goto end;
     }
 
-    err = mWindow->set_buffers_geometry(mWindow, mStride, mScanline, mDisplayFormat);
+    err = mWindow->set_buffers_geometry(mWindow, mWidth, mHeight, mDisplayFormat);
     if (err != 0) {
          ALOGE("%s: set_buffers_geometry failed: %s (%d)",
-               __func__, strerror(-err), -err);
-         ret = UNKNOWN_ERROR;
-         goto end;
-    }
-
-    err = mWindow->set_crop(mWindow, 0, 0, mWidth, mHeight);
-    if (err != 0) {
-         ALOGE("%s: set_crop failed: %s (%d)",
                __func__, strerror(-err), -err);
          ret = UNKNOWN_ERROR;
          goto end;

xoo2001/android_device_lenovo_a6000

camera/QCamera2/HAL/QCameraMem.cpp
@@ -1645,17 +1645,9 @@ int QCameraGrallocMemory::allocate(uint8_t count, size_t /*size*/)
          goto end;
     }
 
-    err = mWindow->set_buffers_geometry(mWindow, mStride, mScanline, mFormat);
+    err = mWindow->set_buffers_geometry(mWindow, mWidth, mHeight, mFormat);
     if (err != 0) {
          ALOGE("%s: set_buffers_geometry failed: %s (%d)",
-               __func__, strerror(-err), -err);
-         ret = UNKNOWN_ERROR;
-         goto end;
-    }
-
-    err = mWindow->set_crop(mWindow, 0, 0, mWidth, mHeight);
-    if (err != 0) {
-         ALOGE("%s: set_crop failed: %s (%d)",
                __func__, strerror(-err), -err);
          ret = UNKNOWN_ERROR;
          goto end;